4f0339b631896bab8aa26bb2a5eed08e9d0a2f98
takashi.ogishima <takashi.ogishima@kaonavi.jp>
2024-08-30 19:44:10 +0900

    boilerplateのlaravelを11に変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::group(['prefix' => 'admin'], function () {
-    Auth::routes([
-        'register' => false
-    ]);
-    Route::get('/', fn() => redirect('/admin/dashboard'));
-    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
 });
