45ae6047921dab6337953f7c82085e1877368866
takashi.ogishima <takashi.ogishima@kaonavi.jp>
2024-08-30 19:57:17 +0900

    larastan追加

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,13 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected static string $factory = UserFactory::class;
 
     /**
      * The attributes that are mass assignable.
